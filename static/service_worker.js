07ea569b11721662877180a8bdd6f115970f50f3
Joshua Byrd <phocks@gmail.com>
2024-02-13 08:20:36 +1000

    Does this work?

--- static/service_worker.js
+++ static/service_worker.js
@@ -7,3 +7,12 @@ self.addEventListener("install", event => {
 self.addEventListener("activate", event => {
   console.log("Service worker activated");
 });
+
+// Listen for online and offline events
+self.addEventListener('online', () => {
+  console.log('Connection is now online.');
+});
+
+self.addEventListener('offline', () => {
+  console.log('Connection is now offline.');
+});
