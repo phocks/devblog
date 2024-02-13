d4f87a10199a507c66eb53f0c077b9b9e2324031
Joshua Byrd <phocks@gmail.com>
2024-02-13 10:50:17 +1000

    OK online offline don't work in sw

--- static/service_worker.js
+++ static/service_worker.js
@@ -1,22 +1,14 @@
 console.log("Hello, this message is sent by a service worker");
 
 // This code executes in its own worker or thread
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   console.log("Service worker installed");
 });
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   console.log("Service worker activated");
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   // Handle fetch events
-});
-
-// Listen for online and offline events
-self.addEventListener('online', () => {
-  console.log('Connection is now online.');
-});
-
-self.addEventListener('offline', () => {
-  console.log('Connection is now offline.');
+  console.log("Fetch event detected");
 });
