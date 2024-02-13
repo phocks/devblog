cb5c507d4679e8842189e10da3eeb68dac286f48
Joshua Byrd <phocks@gmail.com>
2024-02-13 10:51:06 +1000

    Give me back my main

--- static/index.js
+++ static/index.js
@@ -1,17 +1,17 @@
-import init, { main } from "./pkg/wasm.js";
+import init, { main as mainWasm } from "./pkg/wasm.js";
 
 function supportsServiceWorkers() {
   return "serviceWorker" in navigator;
 }
 
-async function run() {
+async function main() {
   supportsServiceWorkers() &&
     navigator.serviceWorker.register("/service_worker.js");
 
   // Some WebAssembly tests
   await init();
-  main();
+  mainWasm();
 }
 
 // Run the main thread
-run();
+main();
