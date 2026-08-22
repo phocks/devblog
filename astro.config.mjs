838ca24d90d59c82d079519ae8dcfce6313aaf7e
Joshua Byrd <phocks@gmail.com>
2026-08-22 14:05:06 +1000

    Favicon and why are we tracking .astro?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,31 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 
+const oldPosts = ["an-initial-test-post"];
+const redirects = Object.fromEntries(
+	oldPosts.map((slug) => [`/${slug}`, `/posts/${slug}`]),
+);
+
 // https://astro.build/config
 export default defineConfig({
-   vite: {
-    plugins: [tailwindcss()],
-  },
-  markdown: {
-    drafts: true,
-    shikiConfig: {
-      theme: "css-variables"
-    }
-  },
-  shikiConfig: {
-    wrap: true,
-    skipInline: false,
-    drafts: true
-  },
-  site: 'https://josh.is-cool.dev',
-  integrations: [sitemap(), mdx()]
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	markdown: {
+		drafts: true,
+		shikiConfig: {
+			theme: "css-variables",
+		},
+	},
+	shikiConfig: {
+		wrap: true,
+		skipInline: false,
+		drafts: true,
+	},
+	site: "https://josh.is-cool.dev",
+	integrations: [sitemap(), mdx()],
+	redirects,
 });
